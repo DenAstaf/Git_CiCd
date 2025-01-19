7656713e8c1bd426a0aecb03a1a7947762bf48f2
Den <otkrytka-m@bk.ru>
2025-01-19 14:47:28 +0500

    add pytest 3

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,18 +5,21 @@ client = TestClient(app)
 
 
 def test_read_root():
+    """Тестирует маршрут корневого пути."""
     response = client.get("/")
     assert response.status_code == 200
     assert response.text == '"Hello, World!"'
 
 
 def test_read_json():
+    """Тестирует маршрут, возвращающий JSON ответ."""
     response = client.get("/json")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, World in JSON!"}
 
 
 def test_read_templated_html():
+    """Тестирует маршрут, использующий шаблон Jinja2 для формирования ответа."""
     response = client.get("/html")
     assert response.status_code == 200
     assert "Hello, Templated World!" in response.text
@@ -24,5 +27,6 @@ def test_read_templated_html():
 
 
 def test_read_fake_route():
+    """Тестирует несуществующий маршрут."""
     response = client.get("/fake")
     assert response.status_code == 404
